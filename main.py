12255c2eef1b3776d8d5ea040f8c6c93beb5eafc
kraulk <kraulk@gmail.com>
2018-01-23 14:07:56 -0500

    creates user class, adds hashutils.py for later security functionality

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, redirect, render_template, flash, session
 from flask_sqlalchemy import SQLAlchemy
+from hashutils import make_pw_hash, check_pw_hash
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'mysecretkeyis'
@@ -16,6 +17,17 @@ class Blog(db.Model):
 	def __init__(self, title, content):
 		self.title = title
 		self.content = content
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	username = db.Column(db.String(120))
+	# TODO use pw hash for security reasons
+	password = db.Colum(db.String(999))
+	blogs = db.relationship('Blog', backref = 'id')
+
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
 
 
 def is_empty(text):
